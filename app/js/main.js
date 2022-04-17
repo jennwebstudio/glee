91e1fe89a0856f83bafe8f5c74a30a590cbe5eef
jennwebstudio <evgenia.ivanova_ru@yahoo.com>
2022-04-17 21:15:47 +0400

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 $(function () {
   
+  $('.star').rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
+  $('.button-list').on('click', function () {
+  $('.products-week__item').addClass('products-week__item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+  $('.products-week__item').removeClass('products-week__item--list'); 
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+  type: "double",
+  prefix: "$",
+  onStart: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+  onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+        },
+});
+
+
   $('.top-slider__inner').slick({
   dots: true,
   arrows: false,
