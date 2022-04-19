35efc799ffa19a292563a3afa5b77ea68262d147
jennwebstudio <evgenia.ivanova_ru@yahoo.com>
2022-04-19 20:20:19 +0400

    added part of product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function () {
   
+
+  $('.product-one__num').styler();
+
+  $('.product-slide__thumb').slick({
+  asNavFor: '.product-slide__big',
+  focusOnSelect: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  vertical: true,
+  draggable: false
+});
+
+$('.product-slide__big').slick({
+  asNavFor: '.product-slide__thumb',
+  draggable: false,
+  arrows: false,
+  fade: true
+});
+
   $('.star').rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
