cac3034666ac4bb9d1d53090f7ae08076f0847a0
jennwebstudio <evgenia.ivanova_ru@yahoo.com>
2022-04-20 19:30:45 +0400

    added second part of ptoduct page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
   
+  $('.related-slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/arrow-left-gray.svg"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/arrow-right-gray.svg"></button>',
+  });
+
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
 
   $('.product-one__num').styler();
 
