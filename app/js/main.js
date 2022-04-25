e1507b3d72a9faa3b44a4b0e861a98514112d5c0
jennwebstudio <evgenia.ivanova_ru@yahoo.com>
2022-04-25 17:30:54 +0400

    made adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function () {
   
+  $('.user-nav__bento').on('click', function () {
+  $('.menu__list').toggleClass('menu__list--active');
+});
+
   $('.related-slider').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/arrow-left-gray.svg"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/arrow-right-gray.svg"></button>',
